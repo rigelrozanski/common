94627a45c05418b024ec894118bbc82133b6ea79
rigelrozanski <rigel.rozanski@gmail.com>
2017-12-11 23:51:46 +0000

    added comand

---
+++ command.go
@@ -0,0 +1,10 @@
+package common
+
+import "os/exec"
+
+// Execute - Execute the command, return standard output and error
+func Execute(command string) (stdOut string, err error) {
+	var outByte []byte
+	outByte, err = exec.Command(command).Output()
+	stdOut = string(outByte)
+}
