2846a52b2a537e9ed8f32fabe468a65eb7bae279
rigelrozanski <rigel.rozanski@gmail.com>
2017-12-12 00:14:32 +0000

    args on execute

--- command.go
+++ command.go
@@ -3,9 +3,9 @@ package common
 import "os/exec"
 
 // Execute - Execute the command, return standard output and error
-func Execute(command string) (stdOut string, err error) {
+func Execute(command string, args ...string) (stdOut string, err error) {
 	var outByte []byte
-	outByte, err = exec.Command(command).Output()
+	outByte, err = exec.Command(command, args).Output()
 	stdOut = string(outByte)
 	return
 }
