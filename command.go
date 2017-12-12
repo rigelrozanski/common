8753c78d2f13758ce020a8a4cb1015ce316ace2e
rigelrozanski <rigel.rozanski@gmail.com>
2017-12-12 00:16:47 +0000

    AsdfasdF

--- command.go
+++ command.go
@@ -5,7 +5,7 @@ import "os/exec"
 // Execute - Execute the command, return standard output and error
 func Execute(command string, args ...string) (stdOut string, err error) {
 	var outByte []byte
-	outByte, err = exec.Command(command, args).Output()
+	outByte, err = exec.Command(command, args...).Output()
 	stdOut = string(outByte)
 	return
 }
