32579ba6868650806d0611ce1b28d5c613809f37
rigelrozanski <rigel.rozanski@gmail.com>
2018-04-11 00:42:23 -0400

    ...

--- command.go
+++ command.go
@@ -2,71 +2,85 @@ package common
 
 import (
 	"errors"
+	"io"
 	"os/exec"
 	"strings"
-	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
-// Execute the command, return standard output and error
-func ExecuteT(t *testing.T, command string) (out string) {
+//// Execute the command, return standard output and error
+//func ExecuteT(t *testing.T, command string) (out string) {
 
-	//split command into command and args
-	var outByte []byte
-	split := strings.Split(command, " ")
-	require.NotZero(t, len(split), "no command provided")
-	cmd := exec.Command(split[:]...)
-	bz, err := cmd.CombinedOutput()
-	require.NoError(t, err)
-	out = strings.Trim(string(bz), "\n") //trim any new lines
-	return out
-}
+////split command into command and args
+//var outByte []byte
+//split := strings.Split(command, " ")
+//require.NotZero(t, len(split), "no command provided")
+//cmd := exec.Command(split[:]...)
+//bz, err := cmd.CombinedOutput()
+//require.NoError(t, err)
+//out = strings.Trim(string(bz), "\n") //trim any new lines
+//return out
+//}
 
-// Asynchronously execute the command, return standard output and error
-func GoExecuteT(t *testing.T, command string) (process *exec.Process, out chan string) {
-	//split command into command and args
-	var outByte []byte
-	split := strings.Split(command, " ")
-	require.NotZero(t, len(split), "no command provided")
-	cmd := exec.Command(split[:]...)
-	go func() {
-		bz, err = cmd.CombinedOutput()
-		require.NoError(t, err)
-	}()
-	out = strings.Trim(string(bz), "\n") //trim any new lines
-	return cmd.Process, stdOut
-}
+//// Asynchronously execute the command, return standard output and error
+//func GoExecuteT(t *testing.T, command string) (process *exec.Process, out chan string) {
+////split command into command and args
+//var outByte []byte
+//split := strings.Split(command, " ")
+//require.NotZero(t, len(split), "no command provided")
+//cmd := exec.Command(split[:]...)
+//go func() {
+//bz, err = cmd.CombinedOutput()
+//require.NoError(t, err)
+//}()
+//out = strings.Trim(string(bz), "\n") //trim any new lines
+//return cmd.Process, stdOut
+//}
 
 //___________________________________________________________________________________________-
 
 // Execute the command, return standard output and error
 func Execute(command string) (out string, err error) {
 	//split command into command and args
-	var outByte []byte
 	split := strings.Split(command, " ")
-	if len(split) == 0 {
-		return nil, "", errors.New("no command provided")
+
+	var cmd *exec.Cmd
+	switch len(split) {
+	case 0:
+		return "", errors.New("no command provided")
+	case 1:
+		cmd = exec.Command(split[0], split[:]...)
+	default:
+		cmd = exec.Command(split[0], split[1:]...)
 	}
-	cmd := exec.Command(split[:]...)
 	bz, err := cmd.CombinedOutput()
 	out = strings.Trim(string(bz), "\n") //trim any new lines
 	return out, err
 }
 
 // Asynchronously execute the command, return standard output and error
-func GoExecute(command string) (process *exec.Process, out chan string, err chan error) {
+func GoExecute(command string) (pipe io.WriteCloser, outChan chan string, errChan chan error, err error) {
 	//split command into command and args
-	var outByte []byte
-	var cmd *exec.Cmd
 	split := strings.Split(command, " ")
-	if len(split) == 0 {
-		return nil, "", errors.New("no command provided")
+
+	var cmd *exec.Cmd
+	switch len(split) {
+	case 0:
+		err = errors.New("no command provided")
+		return nil, outChan, errChan, err
+	case 1:
+		cmd = exec.Command(split[0], split[:]...)
+	default:
+		cmd = exec.Command(split[0], split[1:]...)
 	}
-	cmd = exec.Command(split[:]...)
+	pipe, err = cmd.StdinPipe()
+	if err != nil {
+		err = errors.New("no command provided")
+		return nil, outChan, errChan, err
+	}
 	go func() {
-		bz, err = cmd.CombinedOutput()
+		bz, err := cmd.CombinedOutput()
+		errChan <- err
+		outChan <- strings.Trim(string(bz), "\n") //trim any new lines
 	}()
-	out = strings.Trim(string(bz), "\n") //trim any new lines
-	return cmd.Process, stdOut, err
+	return pipe, outChan, errChan, nil
 }
--- common_test.go
+++ common_test.go
@@ -1,16 +1,41 @@
 package common
 
 import (
+	"encoding/json"
+	"fmt"
+	"strings"
 	"testing"
-
-	"github.com/stretchr/testify/require"
+	//"github.com/stretchr/testify/require"
 )
 
 func TestExecuteCmds(t *testing.T) {
-	proc, outChan, errChan := GoExecute("democoind start")
-	defer proc.Kill()
-	out, err := Execute("sleep 5")
-	require.NoError(t, err)
-	out, err := Execute("democli account C2F2E199A0CE9C7809DDD0EFF774C7DCB4529C26")
-	require.NoError(t, err)
+	out, _ := Execute("basecoind init")
+	//require.NoError(t, err)
+	var initRes map[string]string
+	out = "{" + strings.SplitN(out, "{", 2)[1]
+	_ = json.Unmarshal([]byte(out), &initRes)
+	moneyKey := initRes["secret"]
+	fmt.Println(moneyKey)
+
+	wc, outChan, errChan, err := GoExecute("basecli keys add rigey --recover")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	wc.Write([]byte(moneyKey + "\n")) // XXX why dis enter no work
+	wc.Write([]byte(moneyKey + "\n"))
+	wc.Close()
+	err = <-errChan
+	fmt.Println(err)
+	out = <-outChan
+	fmt.Println(out)
+
+	//out, err := Execute(" init")
+
+	//proc, outChan, errChan := GoExecute("basecoind start")
+	//defer proc.Kill()
+	//out, err := Execute("sleep 5")
+	//require.NoError(t, err)
+	//out, err := Execute("democli account C2F2E199A0CE9C7809DDD0EFF774C7DCB4529C26")
+	//require.NoError(t, err)
 }
