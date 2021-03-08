f0edd794b87dff77bf124fa534f3b99c40015542
rigelrozanski <rigel.rozanski@gmail.com>
2021-03-07 19:01:18 -0500

    parse struct returns field

--- parse/struct.go
+++ parse/struct.go
@@ -12,19 +12,30 @@ type ParsedStruct struct {
 	EndLine   int
 }
 
-// GetParsedStruct
-func GetCurrentParsedStruct(file string, lineNo int) (strct ParsedStruct, found bool) {
+// get the struct (if it exists) containing the lineNo within the given file.
+// If the line number is over a field within the given struct, also retrieve
+// that particular field
+func GetCurrentParsedStruct(file string, lineNo int) (
+	strct ParsedStruct, field ParsedField, found bool) {
+
 	pfile := ParseFile(file)
 	for _, st := range pfile.parsedStructs {
 		if st.StartLine <= lineNo && lineNo <= st.EndLine {
-			return st, true
+			for _, fld := range st.Fields {
+				if fld.StartLine == lineNo {
+					return st, fld, true
+				}
+			}
+			return st, ParsedField{}, true
 		}
 	}
-	return strct, false
+	return ParsedStruct{}, ParsedField{}, false
 }
 
 // NewParsedStruct creates a new ParsedStruct object
-func NewParsedStruct(name string, fields []ParsedField, comment []string, startLine int, endLine int) ParsedStruct {
+func NewParsedStruct(name string, fields []ParsedField,
+	comment []string, startLine int, endLine int) ParsedStruct {
+
 	return ParsedStruct{
 		Name:      name,
 		Fields:    fields,
